9747be698ada14f09268746abf9346a33ee4163e
pacarescuba <94604499+pacarescuba@users.noreply.github.com>
2025-01-13 12:44:48 +0100

    Added solution for Exercise24

--- src/main/java/com/campusdual/classroom/Exercise24.java
+++ src/main/java/com/campusdual/classroom/Exercise24.java
@@ -1,18 +1,22 @@
 package com.campusdual.classroom;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Exercise24 {
-    public static Queue<String> createQueue() {
+  public static Queue<String> createQueue() {
+    return new LinkedList<>(Arrays.asList("Smith", "Montessori", "Peralta", "House"));
+  }
 
+  public static void printAndEmptyQueue(Queue<String> queue) {
+    while (!queue.isEmpty()) {
+      System.out.println(queue.remove());
     }
-    public static void printAndEmptyQueue(Queue<String> queue) {
+  }
 
-    }
-
-    public static void main(String[] args) {
-
-    }
-
+  public static void main(String[] args) {
+    printAndEmptyQueue(new LinkedList<>(Arrays.asList("Smith", "Montessori", "Peralta", "House")));
+  }
 }
